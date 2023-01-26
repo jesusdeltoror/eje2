4fca6ea3589ef7ba0b81ed96d11b1d5834544c94
jesusdeltoror <jesusdeltoror@gmail.com>
2023-01-25 23:27:25 -0600

    Bootcamp eje2

---
+++ index.js
@@ -0,0 +1,15 @@
+let fecha = new Date("June 10, 1983");
+let fecha2 = new Date("1304");
+let lista = [
+    "Jesus Del Toro Robledo",
+    39,
+    true,
+    fecha,
+    {
+        titulo:"La Divina comedia",
+        autor:"Dante Alighieri",
+        fecha:fecha2,
+        url:"https://www.amazon.com.mx/Divina-comedia-Dante-Alighieri/dp/8437632188/ref=asc_df_8437632188/?tag=gledskshopmx-20&linkCode=df0&hvadid=295433315443&hvpos=&hvnetw=g&hvrand=3978990001487110379&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1010040&hvtargid=pla-449711849519&psc=1"
+    }
+]
+
